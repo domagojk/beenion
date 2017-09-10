68f18d1474abad9210c143ab649d80e3614fd2b7
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-09-11 00:56:45 +0200

    permissions tests

--- domain/invariants/permissions.test.ts
+++ domain/invariants/permissions.test.ts
@@ -1,5 +1,5 @@
 import * as permissions from './permissions'
-import { Project, User, Publication } from 'domain/types/model'
+import { Project, User, Publication, Evaluation } from 'domain/types/model'
 
 const genericUser: User = {
   userId: 'test-user-uuid',
@@ -18,42 +18,52 @@ const genericPublication: Publication = {
   publicationId: 'test-publication-uuid',
   privileges: {
     canUpdatePublication: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canDeletePublication: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canCreateProject: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canDeleteProject: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canBanProject: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canUpdateProject: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canResubmitProject: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canVoteWithGold: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canVoteWithSilver: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 100,
       publicationRank: 100
     },
     canVoteWithBronze: {
+      userAccessList: ['special-user-uuid'],
       beenionRank: 10,
       publicationRank: 100
     }
@@ -89,16 +99,16 @@ const genericPublication: Publication = {
   projectStageRules: [
     {
       canReview: {
-        beenionRank: 0,
-        publicationRank: 10
+        beenionRank: 100,
+        publicationRank: 100
       },
       maxReviewers: 3,
       threshold: 2
     },
     {
       canReview: {
-        beenionRank: 0,
-        publicationRank: 10
+        beenionRank: 100,
+        publicationRank: 100
       },
       maxReviewers: 3,
       threshold: 3
@@ -131,14 +141,70 @@ const genericProject: Project = {
   banned: false
 }
 
-describe('project invariants', () => {
-  it('should accept project commands - large beenionRank', () => {
+describe('publication permissions', () => {
+  it('should permit creating publication', () => {
     const user = {
       ...genericUser,
       beenionAnalytics: {
         ...genericUser.beenionAnalytics,
         UserUpvotedWithGold: 100
       }
+    }
+
+    expect(permissions.canCreatePublication(user)).toBe(true)
+  })
+
+  it('should not permit creating publication', () => {
+    const user = {
+      ...genericUser,
+      beenionAnalytics: {
+        ...genericUser.beenionAnalytics,
+        UserDownvotedWithGold: 1
+      }
+    }
+
+    expect(permissions.canCreatePublication(user)).toBe(false)
+  })
+
+  it('should accept publication commands - large beenionRank', () => {
+    const user = {
+      ...genericUser,
+      beenionAnalytics: {
+        ...genericUser.beenionAnalytics,
+        UserUpvotedWithGold: 100
+      }
+    }
+    const publication = {
+      ...genericPublication
+    }
+
+    expect(permissions.canUpdatePublication(user, publication)).toBe(true)
+    expect(permissions.canDeletePublication(user, publication)).toBe(true)
+  })
+
+  it('should accept publication commands - large publicationRank', () => {
+    const user = {
+      ...genericUser,
+      publicationAnalytics: {
+        'test-publication-uuid': {
+          ReviewUpvotedWithGold: 1000
+        }
+      }
+    }
+    const publication = {
+      ...genericPublication
+    }
+
+    expect(permissions.canUpdatePublication(user, publication)).toBe(true)
+    expect(permissions.canDeletePublication(user, publication)).toBe(true)
+  })
+})
+
+describe('project permissions', () => {
+  it('should accept project commands - in user access list', () => {
+    const user = {
+      ...genericUser,
+      userId: 'special-user-uuid'
     }
     const publication = {
       ...genericPublication
@@ -151,6 +217,29 @@ describe('project invariants', () => {
     expect(permissions.canDeleteProject(user, project, publication)).toBe(true)
     expect(permissions.canBanProject(user, project, publication)).toBe(true)
     expect(permissions.canUpdateProject(user, project, publication)).toBe(true)
+  })
+
+  it('should accept project commands - large beenionRank', () => {
+    const user = {
+      ...genericUser,
+      beenionAnalytics: {
+        ...genericUser.beenionAnalytics,
+        UserUpvotedWithGold: 100
+      }
+    }
+    const publication = {
+      ...genericPublication
+    }
+    const project = {
+      ...genericProject,
+      reviewers: ['test-user1-uuid', 'test-user2-uuid']
+    }
+
+    expect(permissions.canCreateProject(user, publication)).toBe(true)
+    expect(permissions.canDeleteProject(user, project, publication)).toBe(true)
+    expect(permissions.canBanProject(user, project, publication)).toBe(true)
+    expect(permissions.canUpdateProject(user, project, publication)).toBe(true)
+    expect(permissions.canInviteReviewer(user, project, publication)).toBe(true)
   })
 
   it('should accept project commands - large publicationRank', () => {
@@ -166,13 +255,15 @@ describe('project invariants', () => {
       ...genericPublication
     }
     const project = {
-      ...genericProject
+      ...genericProject,
+      reviewers: ['test-user1-uuid', 'test-user2-uuid']
     }
 
     expect(permissions.canCreateProject(user, publication)).toBe(true)
     expect(permissions.canDeleteProject(user, project, publication)).toBe(true)
     expect(permissions.canBanProject(user, project, publication)).toBe(true)
     expect(permissions.canUpdateProject(user, project, publication)).toBe(true)
+    expect(permissions.canInviteReviewer(user, project, publication)).toBe(true)
   })
 
   it('should not accept project commands - large beenionRank or publicationRank', () => {
@@ -195,7 +286,6 @@ describe('project invariants', () => {
       ...genericProject
     }
 
-    expect(permissions.canInviteReviewer(user, project, publication)).toBe(false)
     expect(permissions.canResubmitProject(user, project, publication)).toBe(false)
     expect(permissions.canReviewProject(user, project)).toBe(false)
   })
@@ -238,5 +328,127 @@ describe('project invariants', () => {
     }
     expect(permissions.canDeleteProject(user, project, publication)).toBe(true)
     expect(permissions.canUpdateProject(user, project, publication)).toBe(true)
+  })
+
+  it('should permit resubmiting project', () => {
+    const user = {
+      ...genericUser,
+      userId: 'test-user',
+      publicationAnalytics: {
+        'test-publication-uuid': {
+          ReviewUpvotedWithGold: 1000
+        }
+      }
+    }
+    const publication = {
+      ...genericPublication
+    }
+    const project = {
+      ...genericProject,
+      reviewProcessCompleted: true,
+      ownerId: 'test-user'
+    }
+    expect(permissions.canResubmitProject(user, project, publication)).toBe(true)
+  })
+
+  it('should permit removing reviewer', () => {
+    const project = {
+      ...genericProject
+    }
+
+    expect(permissions.canRemoveReviewer(project.reviewers[0], project)).toBe(true)
+  })
+
+  it('should not permit removing reviewer', () => {
+    const project = {
+      ...genericProject
+    }
+
+    expect(permissions.canRemoveReviewer('unlisted-uuid', project)).toBe(false)
+  })
+
+  it('should permit promoting project', () => {
+    const project = {
+      ...genericProject
+    }
+
+    expect(permissions.canPromoteProject(project)).toBe(true)
+  })
+
+  it('should not permit promoting project', () => {
+    const project = {
+      ...genericProject,
+      stageRules: [{
+        ...genericProject.stageRules[0],
+        threshold: 5
+      }]
+    }
+
+    expect(permissions.canPromoteProject(project)).toBe(false)
+  })
+
+  it('should permit approving project', () => {
+    const project = {
+      ...genericProject,
+      currentStage: 1,
+      evaluations: [
+        {
+          reviewerId: 'test-user1-uuid',
+          evaluation: 'approve' as Evaluation
+        },
+        {
+          reviewerId: 'test-user2-uuid',
+          evaluation: 'approve' as Evaluation
+        },
+        {
+          reviewerId: 'test-user3-uuid',
+          evaluation: 'approve' as Evaluation
+        }
+      ]
+    }
+    expect(permissions.canApproveProject(project)).toBe(true)
+  })
+
+  it('should not permit accepting project', () => {
+    const project = {
+      ...genericProject,
+      currentStage: 1,
+      evaluations: [
+        {
+          reviewerId: 'test-user1-uuid',
+          evaluation: 'approve' as Evaluation
+        },
+        {
+          reviewerId: 'test-user2-uuid',
+          evaluation: 'approve' as Evaluation
+        },
+        {
+          reviewerId: 'test-user3-uuid',
+          evaluation: 'reject' as Evaluation
+        }
+      ]
+    }
+    expect(permissions.canApproveProject(project)).toBe(false)
+  })
+
+  it('should permit rejecting project', () => {
+    const project = {
+      ...genericProject,
+      evaluations: [
+        {
+          reviewerId: 'test-user1-uuid',
+          evaluation: 'approve' as Evaluation
+        },
+        {
+          reviewerId: 'test-user2-uuid',
+          evaluation: 'reject' as Evaluation
+        },
+        {
+          reviewerId: 'test-user3-uuid',
+          evaluation: 'reject' as Evaluation
+        }
+      ]
+    }
+    expect(permissions.canRejectProject(project)).toBe(true)
   })
 })
