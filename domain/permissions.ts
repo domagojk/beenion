110b4dee43c9ddd91f9edc72d67b8746d6e52ee7
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-10-02 18:46:17 +0200

    permission cond refractor

--- domain/permissions.ts
+++ domain/permissions.ts
@@ -8,41 +8,32 @@ import {
 } from './types/model'
 
 export const hasBeenionAccess = (perm: BeenionPermission, user: User) =>
-  perm &&
-  (
-    isListedUser(perm, user) ||
-    hasBeenionRank(perm, user)
-  ) || false
+  isListedUser(perm, user) ||
+  hasBeenionRank(perm, user)
 
 export const hasPublicationAccess = (
   perm: PublicationPermission,
   user: User,
   pub: Publication
 ) =>
-  perm &&
-  (
-    isListedUser(perm, user) ||
-    hasBeenionRank(perm, user) ||
-    hasPublicationRank(perm, user, pub)
-  ) || false
+  isListedUser(perm, user) ||
+  hasBeenionRank(perm, user) ||
+  hasPublicationRank(perm, user, pub)
 
 export const isListedUser = (perm: PublicationPermission, user: User) =>
-  perm.users &&
+  !!perm && Array.isArray(perm.users) &&
   perm.users.includes(user.userId)
 
-export const hasBeenionRank = (perm: BeenionPermission, user: User) => {
-  return (
-    perm.beenionRank !== undefined &&
-    calcRank(user.rankEvents, beenionRankCalcParams) >= perm.beenionRank.min
-  )
-}
+export const hasBeenionRank = (perm: BeenionPermission, user: User) =>
+  !!perm && !!perm.beenionRank &&
+  calcRank(user.rankEvents, beenionRankCalcParams) >= perm.beenionRank.min
 
 export const hasPublicationRank = (
   perm: PublicationPermission,
   user: User,
   pub: Publication
 ) =>
-  perm.publicationRank !== undefined &&
+  !!perm && !!perm.publicationRank &&
   calcRank(
     user.rankEvents.filter(e =>
       e.publicationId === undefined ||
