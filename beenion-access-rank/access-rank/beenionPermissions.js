762237a316725c4224dbe0e30bd4aba311f63f6a
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-11-15 22:38:57 +0100

    remove access rank

--- beenion-access-rank/access-rank/beenionPermissions.js
+++
@@ -1,9 +0,0 @@
-const beenionPermissions = {
-  createJournal: { globalRank: 10 },
-  deleteJournal: { globalRank: 5000 },
-  rateUserWithGold: { globalRank: 1000 },
-  rateUserWithSilver: { globalRank: 500 },
-  rateUserWithBronze: { globalRank: 100 }
-}
-
-module.exports = beenionPermissions
--- beenion-access-rank/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-router.get('/', function(req, res, next) {
-  res.send('hello')
-})
-
-module.exports = router
