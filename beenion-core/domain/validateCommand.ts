1dcf4bbe61a78cae899a0a0db8a987ba12514f14
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-12-28 11:37:01 +0100

    added error reporter

--- beenion-core/domain/validateCommand.ts
+++ beenion-core/domain/validateCommand.ts
@@ -1,10 +1,13 @@
 import * as t from 'io-ts'
 import { Type } from 'io-ts/lib'
+import { failure } from 'io-ts/lib/PathReporter'
 
 const validateCommand = <T>(command: object, type: Type<T>): T => {
   return t.validate(command, type).fold(
     (err) => {
-      throw new TypeError(err[0].value)
+      const errors = failure(err)
+      // throwing only first error
+      throw errors[0]
     },
     data => data
   )
