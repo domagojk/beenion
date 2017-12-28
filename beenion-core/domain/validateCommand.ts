3ab7d91e830af49202da734a05a66de351a72a8f
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2017-12-28 13:30:11 +0100

    cleaned validation report

--- beenion-core/domain/validateCommand.ts
+++ beenion-core/domain/validateCommand.ts
@@ -1,16 +1,22 @@
 import * as t from 'io-ts'
 import { Type } from 'io-ts/lib'
-import { failure } from 'io-ts/lib/PathReporter'
+
+function stringify (v: any): string {
+  return typeof v === 'function' ? t.getFunctionName(v) : JSON.stringify(v)
+}
+
+function getContextPath (context: t.Context): string {
+  return context.map(({ key, type }) => `${key}`).join('.')
+}
+
+function getMessage (v: any, context: t.Context): string {
+  return `Invalid value ${stringify(v)} supplied to command${getContextPath(context)}`
+}
 
 const validateCommand = <T>(command: object, type: Type<T>): T => {
-  return t.validate(command, type).fold(
-    (err) => {
-      const errors = failure(err)
-      // throwing only first error
-      throw errors[0]
-    },
-    data => data
-  )
+  return t.validate(command, type).fold(err => {
+    throw err.map(e => getMessage(e.value, e.context))
+  }, data => data)
 }
 
 export default validateCommand
