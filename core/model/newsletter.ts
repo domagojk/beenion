b7090c28774d732b94f433022767c96502883f22
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2018-02-26 21:14:36 +0100

    refractor

--- core/model/newsletter.ts
+++ core/model/newsletter.ts
@@ -1,10 +1,13 @@
-/* newsletter {
-  id
-  owners: userId[]
-  stages: []
-  maxReviewers: 500
+import * as t from 'io-ts'
+
+const CreateNewsletter = t.type({
+  newsletterId: t.string,
+  editors: t.array(t.string)
+})
+
+type Stage = {
+  
 }
-*/
 
 type Newsletter = {
   newsletterId: string
