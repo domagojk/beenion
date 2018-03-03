7fcab7830a53122ed09bc6c5fa4ce70b373b3477
Domagoj Kriskovic <domagoj.kriskovic@gmail.com>
2018-03-03 17:04:12 +0100

    link api, assign reviewers

--- core/model/link.ts
+++ core/model/link.ts
@@ -1,9 +1,10 @@
 import * as err from './errors'
-import { User } from './user'
-import { newsletterApiFactory, NewsletterRepository } from './newsletter'
+import { User, UserId } from './user'
+import { NewsletterApi } from './newsletter'
+import { LinkRepository } from '../repositories/link'
+import { SubscriberRepository } from '../repositories/subscriber'
 
-type UserId = User['userId']
-type Evaluation = null | 'accepted' | 'rejected'
+export type Evaluation = null | 'accepted' | 'rejected'
 
 export type Link = {
   linkId: string
@@ -17,109 +18,158 @@ export type Link = {
   reviews: {
     [key in UserId]: {
       stage: number
-      evaluation: Evaluation
+      evaluation: null | 'accepted' | 'rejected'
     }
   }
 }
 
-export type CreateLink = {
-  linkId: string
-  newsletterId: string
-  title: string
-  description: string
-  url: string
+export type LinkApi = {
+  get(linkId: string): Promise<Link>
+  create(
+    user: User,
+    params: {
+      linkId: string
+      newsletterId: string
+      title: string
+      description: string
+      url: string
+    }
+  ): Promise<Link>
+  updateMetadata(
+    user: User,
+    params: {
+      linkId: string
+      title?: string
+      description?: string
+      url?: string
+    }
+  ): Promise<Link>
+  del(user: User, linkId: string): Promise<Link>
+  assignReviewers(linkId: string): Promise<Link>
+  review(
+    user: User,
+    params: {
+      linkId: string
+      evaluation: Evaluation
+    }
+  ): Promise<Link>
 }
 
-export type UpdateLinkMetadata = {
-  linkId: string
-  title?: string
-  description?: string
-  url?: string
-}
-
-export type ReviewLink = {
-  linkId: string
-  evaluation: Evaluation
-}
-
-export type LinkRepository = {
-  get: (id: string) => Promise<Link>
-  save: (id: string, doc: Link) => Promise<Link>
-  update: (id: string, doc: Link) => Promise<Link>
-  delete: (id: string) => Promise<Link>
-}
-
-export function linkApiFactory(
+export const linkApiFactory = (
   linkRepo: LinkRepository,
-  newsletterRepo: NewsletterRepository
-) {
-  const newsletterApi = newsletterApiFactory(newsletterRepo)
-  const linkApi = {
-    get(id: string) {
-      return linkRepo.get(id)
-    },
+  subscriberRepo: SubscriberRepository,
+  newsletterApi: NewsletterApi
+): LinkApi => ({
+  get(linkId) {
+    return linkRepo.get(linkId)
+  },
 
-    create(user: User, command: CreateLink) {
-      return linkRepo.save(command.linkId, {
-        ...command,
+  async create(user, params) {
+    const newsletter = await newsletterApi.get(params.newsletterId)
+    const subscriber = await subscriberRepo.findSubscriber({
+      subscriberId: user.userId,
+      newsletterId: params.newsletterId
+    })
+
+    if (!subscriber || !newsletter.editors.includes(user.userId)) {
+      throw err.accessDenied()
+    }
+
+    return linkRepo.save(
+      params.linkId,
+      {
+        ...params,
         userId: user.userId,
         status: 'pending',
         stage: 0,
         reviews: {}
-      })
-    },
+      },
+      'LINK_CREATED'
+    )
+  },
 
-    async updateMetadata(user: User, command: UpdateLinkMetadata) {
-      const link = await linkApi.get(command.linkId)
-      const newsletter = await newsletterApi.get(link.newsletterId)
+  async updateMetadata(user, params) {
+    const link = await linkRepo.get(params.linkId)
+    const newsletter = await newsletterApi.get(link.newsletterId)
 
-      if (
-        link.userId !== user.userId ||
-        !newsletter.editors.includes(user.userId)
-      ) {
-        throw err.accessDenied()
-      }
+    if (
+      link.userId !== user.userId ||
+      !newsletter.editors.includes(user.userId)
+    ) {
+      throw err.accessDenied()
+    }
 
-      return linkRepo.update(newsletter.newsletterId, {
-        ...link,
-        ...command
-      })
-    },
+    return linkRepo.update(params.linkId, params, 'LINK_METADATA_UPDATED')
+  },
 
-    async del(user: User, linkId: string) {
-      const link = await linkApi.get(linkId)
+  async del(user, linkId) {
+    const link = await linkRepo.get(linkId)
 
-      if (link.userId !== user.userId) {
-        throw err.accessDenied()
-      }
+    if (link.userId !== user.userId) {
+      throw err.accessDenied()
+    }
 
-      return linkRepo.delete(linkId)
-    },
+    return linkRepo.delete(linkId, 'LINK_DELETED')
+  },
 
-    async review(user: User, command: ReviewLink) {
-      const link = await linkApi.get(command.linkId)
-      const linkReview = link.reviews[user.userId]
+  async assignReviewers(linkId) {
+    const link = await linkRepo.get(linkId)
+    const newsletter = await newsletterApi.get(link.newsletterId)
 
-      if (!linkReview) {
-        throw err.accessDenied()
-      }
+    // get only reviewers assigned for current stage
+    const stageReviewers = Object.keys(link.reviews).filter(
+      userId => link.reviews[userId].stage === link.stage
+    )
 
-      if (linkReview.stage !== link.stage) {
-        throw err.accessDenied()
-      }
+    const reviewers = await subscriberRepo.findReviewer({
+      linkId: link.linkId,
+      newsletterId: newsletter.newsletterId,
+      minSimilarityRank: newsletter.stages[link.stage].minSimilarityRank,
+      exclude: [...stageReviewers, link.userId],
+      limit: newsletter.stages[link.stage].minReviews - stageReviewers.length
+    })
 
-      return linkRepo.update(link.linkId, {
-        ...link,
+    return linkRepo.update(
+      link.linkId,
+      {
+        reviews: reviewers.reduce(
+          (acc, reviewer) => ({
+            ...acc,
+            [reviewer]: {
+              stage: link.stage,
+              evaluation: null
+            }
+          }),
+          link.reviews
+        )
+      },
+      'LINK_REVIEWERS_ASSIGNED'
+    )
+  },
+
+  async review(user, params) {
+    const link = await linkRepo.get(params.linkId)
+    const linkReview = link.reviews[user.userId]
+
+    if (!linkReview) {
+      throw err.accessDenied()
+    }
+
+    if (linkReview.stage !== link.stage) {
+      throw err.accessDenied()
+    }
+
+    return linkRepo.update(
+      link.linkId,
+      {
         reviews: {
-          ...link.reviews,
           [user.userId]: {
             stage: link.stage,
-            evaluation: command.evaluation
+            evaluation: params.evaluation
           }
         }
-      })
-    }
+      },
+      'LINK_REVIEWED'
+    )
   }
-
-  return linkApi
-}
+})
