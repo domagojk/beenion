67214ed53bfad1f794ec9a06644c221efeb94bad
Domagoj <domagoj.kriskovic@gmail.com>
2019-02-20 21:45:15 +0100

    update

--- infrastructure/databases/eventstore/getSyncTime.ts
+++ infrastructure/databases/eventstore/getSyncTime.ts
@@ -1,9 +1,15 @@
-import ntpClient from 'ntp-client'
+// import ntpClient from 'ntp-client'
 
-const timeSyncEndpoint = process.env.NTP_URL || '169.254.169.123'
+// const timeSyncEndpoint = process.env.NTP_URL || '169.254.169.123'
+
+// NOTE
+// removed ntp sync time since it stopped working in lambda
+// todo: it should be invastigated why
 
 export function getSyncTime() {
   return new Promise((resolve, reject) => {
+    resolve(Date.now())
+    /*
     ntpClient.getNetworkTime(timeSyncEndpoint, 123, function(err, date) {
       if (err) {
         reject(err)
@@ -13,5 +19,6 @@ export function getSyncTime() {
       const jsDate = new Date(date)
       resolve(jsDate.getTime())
     })
+    */
   })
 }
